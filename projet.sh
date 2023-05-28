6bdf208ef1ecb4163c0d0f53631e19cab46c5e41
AlexandreGirolt <alexandre.girolt@sfr.fr>
2023-05-28 21:30:52 +0200

    Installation Eclipse + pare feu

--- projet.sh
+++ projet.sh
@@ -47,3 +47,35 @@ do
 	ssh agirol25@10.30.48.100 "mail --subject \"Création de votre compte\" --exec \"set sendmail=smtp://${mail_login/@/%40}:mail_password@$server:$mail_port\" --append \"From:$mail_login\" $mail <<< \"Votre compte à été créé avec succès! Voici vos identifiants : \nMail : $mail\nPassword : $password\nUne fois connecté(e) avec ce mot de passe, il vous sera demandé d'en choisir un nouveau.\""
 
 done
+
+
+########################################################################
+#############################  Sauvegarde  #############################
+########################################################################
+
+#Création du dossier saves sur la machine distante
+ssh agirol25@10.30.48.100 "sudo mkdir /home/saves"
+ssh agirol25@10.30.48.100 "sudo chmod 766 \"/home/saves\""
+
+
+########################################################################
+#######################  Installation Eclipse  #########################
+########################################################################
+
+apt install snapd -y
+ln -s /var/lib/snapd/snap /snap
+snap refresh
+snap install --classic eclipse
+
+
+########################################################################
+#############################  Pare feu  ###############################
+########################################################################
+
+iptables -A INPUT -p tcp --dport 21 -j DROP
+iptables -A OUTPUT -p tcp --dport 21 -j DROP
+iptables -A INPUT -p udp -j DROP
+iptables -A OUTPUT -p udp -j DROP
+iptables-save > /etc/iptables/rules.v4
+
+
